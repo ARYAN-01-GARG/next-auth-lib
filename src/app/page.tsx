e1ccfacda84757ca8d9009391ca76a1da8422912
ARYAN-01-GARG <aryangarg30842@gmail.com>
2024-12-30 02:10:26 +0530

    Added shadcn and written some new imortant component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="">
-      Hello
+    <div className="flex h-full items-center justify-center">
+      <Button>
+        Click me
+      </Button>
     </div>
   );
 }
