7c28c62615c7b6d1ae2712ae61e2653fcb39bacd
ARYAN-01-GARG <aryangarg30842@gmail.com>
2025-03-15 11:44:45 +0530

    Added prisma orm and schema

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -5,6 +5,7 @@ import { RegisterSchema } from "@/schemas";
 
 export const register = async (values : z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
+    console.log(validatedFields);
     if(!validatedFields) {
         return { error : 'All Fields are required!'}
     }
